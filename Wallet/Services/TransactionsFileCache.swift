d901e9a0f82d555b8332cbc5b2be03d666c1e9f0
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-06-10 17:02:10 +0200

    Finished TransactionsFileCache class

--- Wallet/Services/TransactionsFileCache.swift
+++ Wallet/Services/TransactionsFileCache.swift
@@ -19,10 +19,37 @@ class TransactionsFileCache {
     }
     
     //TODO: Сохранение всех операций в файл в формате JSON
-    public func save(to filename: String) {}
+    public func save(to filename: String) {
+        let array: [Any] = transactions.map { $0.jsonObject }
+        
+        do {
+            let data = try JSONSerialization.data(withJSONObject: array, options: [.prettyPrinted])
+            let url = getURL(for: filename)
+            try data.write(to: url)
+        } catch {
+            print("Ошибка при сохранении: \(error)")
+        }
+    }
+    
+    private func getURL(for filename: String) -> URL {
+        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        return directory.appendingPathComponent(filename).appendingPathExtension("json")
+    }
     
     //TODO: Загрузка всех операций из файла в формате JSON
-    public func load(from filename: String) {}
+    public func load(from filename: String) {
+        let url = getURL(for: filename)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            
+            let rawArray = try JSONSerialization.jsonObject(with: data) as? [Any] ?? []
+            
+            transactions = rawArray.compactMap { Transaction.parse(jsonObject: $0) }
+        } catch {
+            print("Ошибка при загрузке: \(error)")
+        }
+    }
     
     
 }
